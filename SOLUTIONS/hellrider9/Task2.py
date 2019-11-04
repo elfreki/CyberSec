c581cffb4f4895ede3d9023923ec276b889f45c6
hellrider9 <56410386+hellrider9@users.noreply.github.com>
2019-11-05 04:00:15 +0530

    added comments

--- SOLUTIONS/hellrider9/Task2.py
+++ SOLUTIONS/hellrider9/Task2.py
@@ -1,14 +1,14 @@
-def permute(a, l, r): 
-    f=open("text.txt","a+")
+def permute(a, l, r):             #function to permutate the given words
+    f=open("text.txt","a+")       #opening a file stream to write data to
     if l==r: 
         for i in a:
-          f.write(i)
+          f.write(i)              #writing output onto the file
         f.write("\n")  
     else: 
-        for i in range(l,r+1): 
-            a[l], a[i] = a[i], a[l] 
-            permute(a, l+1, r) 
-            a[l], a[i] = a[i], a[l] 
-strings=["pr0","n00b","f1r3","h4wk","fl4sh1r","r3b00t","p4r4","d0x"]
-permute(strings, 0, 7) 
+        for i in range(l,r+1):                 #iterating over the array
+            a[l], a[i] = a[i], a[l]            #swapping elements
+            permute(a, l+1, r)                 # recursion is being used 
+            a[l], a[i] = a[i], a[l]            #swapping again to revert the changes
+strings=["pr0","n00b","f1r3","h4wk","fl4sh1r","r3b00t","p4r4","d0x"]            #array of strings which will be permutated 
+permute(strings, 0, 7)           #calling the function permute to begin the algorithm
   
