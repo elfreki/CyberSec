b6c87a063aedee77855c76401317d6df38168b33
Aditya Verma <56410386+hellrider9@users.noreply.github.com>
2019-11-07 02:49:38 +0530

    update

--- SOLUTIONS/hellrider9/new task 2 solution/task2.sh
+++ SOLUTIONS/hellrider9/new task 2 solution/task2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 crunch 8 8 -p "f1r3" "h4wk" "pr0" "n00b" "r3b00t" "fl4shir" "p4r4" "d0x" > list.txt
+# here crunch command is used which creates a wordlist. two 8 written after crunch signify that minimum and maximum value of 
+# wordlist to be generated should be 8 respectively. -p checks that permutations are created without repetitions
